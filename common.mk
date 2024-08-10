2c7e5e207cd931adbb225db257b71c075550b15e
luffitys <banackidaniel@gmail.com>
2024-08-10 15:06:36 +0200

    sm8450-common: Build missing Wi-Fi Display lib
    E AndroidRuntime: java.lang.UnsatisfiedLinkError: dlopen failed: library
    "vendor.qti.hardware.display.config-V5-ndk.so" not found: needed by
    /system_ext/lib64/libwfddisplayconfig.so in namespace clns-shared-4
    
    Change-Id: Ic17db866a0fd71f459bcb6c5384aee7ce563f130

--- common.mk
+++ common.mk
@@ -492,7 +492,8 @@ PRODUCT_COPY_FILES += \
 
 # WiFi Display
 PRODUCT_PACKAGES += \
-    libwfdaac_vendor:64
+    libwfdaac_vendor:64 \
+    vendor.qti.hardware.display.config-V5-ndk:64
 
 # WiFi firmware symlinks
 PRODUCT_PACKAGES += \
