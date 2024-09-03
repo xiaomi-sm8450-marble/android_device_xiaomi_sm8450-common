01ff93bff6cde66c29ab01dd30c572e08f5973a7
Anush02198 <anush.4376@gmail.com>
2024-09-03 11:01:59 +0530

    sm8450-common: parts: Import from xiaomi_sdm845-common

--- common.mk
+++ common.mk
@@ -139,6 +139,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
 
+# Device-specific settings
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint-service.xiaomi \
