507ecb7786ea7188389431cd815f88b54a00a276
pabloescobar-reborn <yogivangala07@gmail.com>
2025-08-07 11:43:54 +0000

    sm8450-common: vintf: Import device framework matrix from lineageos
    Based on upstream LineageOS changes (March–June 2025).
    
    Signed-off-by: Pablo Escobar <pabloescobar77@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -215,10 +215,10 @@ $(foreach nfc_sku, $(call to-upper, $(TARGET_NFC_SUPPORTED_SKUS)), \
     $(eval ODM_MANIFEST_$(nfc_sku)_FILES += $(COMMON_PATH)/vintf/manifest_nfc.xml))
 endif
 
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(COMMON_PATH)/vintf/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 
 DEVICE_FRAMEWORK_MANIFEST_FILE += $(COMMON_PATH)/vintf/framework_manifest.xml
 
